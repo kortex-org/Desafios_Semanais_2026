f1f47c6596661a731ce783cb9b0c2dabebe30d4d
Giuliana Lisboa <giulianalcp201206@gmail.com>
2026-04-18 15:55:02 -0300

    Add files via upload

---
+++ Semana 1/resoluções/Giuzs/q3.py
@@ -0,0 +1,15 @@
+pinheiro='''
+ *      *    X        *
+   *        XoX    *      *
+*          XOXXX       
+     *    XXXXOXX       *
+ *      xXOXXXXoXXx  *      *
+    *  xXXXOXXXXXOXx
+*      xXoXXXXXXOXXx   *
+      XXXOXXXoXXXXXOX       *
+    *       XXX     
+ *          XXX         *
+           XXXXX        
+wwwwwwwwwwwwwwwwwwwwwwwwwwwwww
+'''
+print(pinheiro)
