40c2af2ce7e89c897b1c000a2b0e6a4e4dcb5f2d
Daniel Rond de Jesus dos Santos <166262041+DanielRond@users.noreply.github.com>
2026-04-20 00:04:26 -0300

    correcao ola q10

--- Semana 1/resoluções/DanielRond/q10.py
+++ Semana 1/resoluções/DanielRond/q10.py
@@ -4,4 +4,4 @@ Faça um programa que receba um nome, idade e curso e exiba no formato: "Olá, *
 nome = input("Qual seu nome?\n")
 idade = input("Qual sua idade?\n")
 curso = input("Qual seu curso?\n")
-print(f"{nome}! Você tem {idade} anos e faz {curso}")
+print(f"Olá,{nome}! Você tem {idade} anos e faz {curso}")
